7a7ef54e3c4939c7cbc8c6cb68ca64bdea83b978
thelarrywei <thelarrywei@gmail.com>
2014-11-12 13:38:34 -0800

    Initial Commit

---
+++ Tinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tinder
+//
+//  Created by Larry Wei on 11/12/14.
+//  Copyright (c) 2014 Larry Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
